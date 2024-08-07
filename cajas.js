74115f6fdecd016657334e8ac80064b25ca4a3c0
LuisaTorres03 <luisa.dlc.torres@gmail.com>
2024-08-07 16:24:26 -0500

    se agrego el encabezado

--- cajas.js
+++ cajas.js
@@ -6,23 +6,27 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
     const addRowButton = document.getElementById('addRow');
     const addBultoButton = document.getElementById('addBulto');
+    const backButton = document.getElementById('backButton');
+
+    backButton.addEventListener('click', () => {
+        window.history.back();
+    });
 
     addRowButton.addEventListener('click', () => {
         const tableBody = document.querySelector('table tbody');
         const newRow = document.createElement('tr');
 
         newRow.innerHTML = `
-           <td contenteditable="true" oninput="validateNumberInput(this)"></td>
-            <td contenteditable="true"></td>
-           <td contenteditable="true" oninput="validateNumberInput(this)"></td>
             <td contenteditable="true" oninput="validateNumberInput(this)"></td>
-           <td contenteditable="true" oninput="validateNumberInput(this)"></td>
+            <td contenteditable="true"></td>
+            <td contenteditable="true" oninput="validateNumberInput(this)"></td>
+            <td contenteditable="true" oninput="validateNumberInput(this)"></td>
+            <td contenteditable="true" oninput="validateNumberInput(this)"></td>
             <td contenteditable="true">${formattedDate}</td>
-            <td><img src="assets.img/icons8-eliminar-16 (1).png"  alt="Eliminar" onclick="deleteRow(this)"></td>
+            <td><img src="assets.img/icons8-eliminar-16 (1).png" class="delete-icon" alt="Eliminar" onclick="deleteRow(this)"></td>
         `;
         tableBody.appendChild(newRow);
 
-        
         document.querySelector('.date-reg-header').classList.remove('hidden');
     });
 
@@ -37,12 +41,16 @@ document.addEventListener('DOMContentLoaded', (event) => {
             <td contenteditable="false">BULTO</td>
             <td contenteditable="false">BULTO</td>
             <td contenteditable="true">${formattedDate}</td>
-            <td><img src="assets.img/icons8-eliminar-16 (1).png"  alt="Eliminar" onclick="deleteRow(this)"></td>
+            <td><img src="assets.img/icons8-eliminar-16 (1).png" class="delete-icon" alt="Eliminar" onclick="deleteRow(this)"></td>
         `;
         tableBody.appendChild(newRow);
 
-        
         document.querySelector('.date-reg-header').classList.remove('hidden');
+    });
+
+    const aplicacionBtn = document.getElementById('aplicacionBtn');
+    aplicacionBtn.addEventListener('click', () => {
+        alert('BOTÓN APLICACIÓN PRESIONADO');
     });
 
     document.addEventListener('focusout', (e) => {
@@ -54,29 +62,17 @@ document.addEventListener('DOMContentLoaded', (event) => {
         }
     });
 
-    const aplicacionBtn = document.getElementById('aplicacionBtn');
-    aplicacionBtn.addEventListener('click', () => {
-        alert('BOTÓN APLICACIÓN PRESIONADO');
-    });
-
     const matchBtn = document.getElementById('matchBtn');
     matchBtn.addEventListener('click', () => {
         alert('BOTÓN MATCH PRESIONADO');
     });
 });
 
-function validateNumberInput(cell) {
-    cell.textContent = cell.textContent.replace(/[^0-9]/g, '');
+function deleteRow(element) {
+    const row = element.closest('tr');
+    row.parentNode.removeChild(row);
 }
 
-
-function deleteRow(element) {
-    const row = element.parentNode.parentNode;
-    row.parentNode.removeChild(row);
-
-    
-    const tableBody = document.querySelector('table tbody');
-    if (!tableBody.hasChildNodes()) {
-        document.querySelector('.date-reg-header').classList.add('hidden');
-    }
+function validateNumberInput(element) {
+    element.textContent = element.textContent.replace(/[^0-9]/g, '');
 }
