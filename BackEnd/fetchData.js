555a48d7137208b18e579722b323cae050bf6e9b
Roger Cabezudo <roggerjjj@gmail.com>
2024-08-07 12:27:28 -0500

    SIGUE EL ERROR CON EL GET

--- BackEnd/fetchData.js
+++ BackEnd/fetchData.js
@@ -1,15 +1,19 @@
 // fetchData.js
-import { supabase } from './supabaseClient';
+import { Console } from "console";
+import { supabase } from "./supabaseClient";
 
 async function fetchData() {
-  const { data, error } = await supabase
-    .from('TBL_CAJAS')
-    .select('*');
+  let { data: TBL_CAJAS, error } = await supabase.from("TBL_CAJAS").select("*");
 
   if (error) {
-    console.error('Error al obtener datos:', error);
+    console.error("Código de error:", error.code);
+    console.error("Mensaje de error:", error.message);
+    console.error("Detalles:", error.details);
+    console.error("Sugerencias:", error.hint);
     return [];
   }
+
+  console.log("Data:", data);
 
   return data;
 }
