624b908241c5c8de8350f67870f37a3ab0a3eb43
LuisaTorres03 <luisa.dlc.torres@gmail.com>
2024-08-07 17:49:25 -0500

    bug arreglado

--- MODULOS/CAJAS/cajas.js
+++ MODULOS/CAJAS/cajas.js
@@ -8,12 +8,10 @@ document.addEventListener('DOMContentLoaded', (event) => {
     const addBultoButton = document.getElementById('addBulto');
     const backButton = document.getElementById('backButton');
 
-    //Boton para retroceder
     backButton.addEventListener('click', () => {
         window.history.back();
     });
 
-    //Funcion para agregar una nueva fila
     addRowButton.addEventListener('click', () => {
         const tableBody = document.querySelector('table tbody');
         const newRow = document.createElement('tr');
@@ -25,14 +23,13 @@ document.addEventListener('DOMContentLoaded', (event) => {
             <td contenteditable="true" oninput="validateNumberInput(this)"></td>
             <td contenteditable="true" oninput="validateNumberInput(this)"></td>
             <td contenteditable="true">${formattedDate}</td>
-            <td><img src="assets.img/icons8-eliminar-16 (1).png" class="delete-icon" alt="Eliminar" onclick="deleteRow(this)"></td>
+            <td><img src="/assets.img/icons8-eliminar-16 (1).png" class="delete-icon" alt="Eliminar" onclick="deleteRow(this)"></td>
         `;
         tableBody.appendChild(newRow);
 
         document.querySelector('.date-reg-header').classList.remove('hidden');
     });
 
-    //Funcion para agregar un Bulto
     addBultoButton.addEventListener('click', () => {
         const tableBody = document.querySelector('table tbody');
         const newRow = document.createElement('tr');
@@ -44,28 +41,18 @@ document.addEventListener('DOMContentLoaded', (event) => {
             <td contenteditable="false">BULTO</td>
             <td contenteditable="false">BULTO</td>
             <td contenteditable="true">${formattedDate}</td>
-            <td><img src="assets.img/icons8-eliminar-16 (1).png" class="delete-icon" alt="Eliminar" onclick="deleteRow(this)"></td>
+            <td><img src="/assets.img/icons8-eliminar-16 (1).png" class="delete-icon" alt="Eliminar" onclick="deleteRow(this)"></td>
         `;
         tableBody.appendChild(newRow);
 
         document.querySelector('.date-reg-header').classList.remove('hidden');
     });
 
-    //Boton de aplicacion
     const aplicacionBtn = document.getElementById('aplicacionBtn');
     aplicacionBtn.addEventListener('click', () => {
         alert('BOTÓN APLICACIÓN PRESIONADO');
     });
 
-    //
-    document.addEventListener('focusout', (e) => {
-        const cell = e.target;
-        if (cell.cellIndex >= 2 && cell.cellIndex <= 4 && cell.isContentEditable) {
-            if (!cell.textContent.trim().endsWith(" cm") && cell.textContent.trim() !== "BULTO") {
-                cell.textContent = cell.textContent.trim() + " cm";
-            }
-        }
-    });
 
     const matchBtn = document.getElementById('matchBtn');
     matchBtn.addEventListener('click', () => {
