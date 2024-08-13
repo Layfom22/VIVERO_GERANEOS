c52549bed1053c128c455b47df66ae6dc9856214
Roger Cabezudo <roggerjjj@gmail.com>
2024-08-13 16:58:55 -0500

    NO SE PUDO MODIFICAR CON EL .ENV

--- BackEnd/supabaseClient.js
+++ BackEnd/supabaseClient.js
@@ -1,8 +1,12 @@
 // supabaseClient.js
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
+
 // Reemplaza con la URL y la clave de tu proyecto Supabase
 const supabaseUrl = 'https://ivhdwqxxxchhkuhtstjl.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml2aGR3cXh4eGNoaGt1aHRzdGpsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjI2MTM2NjMsImV4cCI6MjAzODE4OTY2M30.sEIiZaU5UEF9KhMJyKN-wn9OdqDLBC3qL1Ss7B1KRMY';
+//const supabaseUrl = process.env.SUPABASEURL;
+//const supabaseKey = process.env.SUPABASEKEY;
+
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
